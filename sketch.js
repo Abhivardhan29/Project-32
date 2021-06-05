8a84dc0dba90320b5451299fb4ab50e666a308ce
Abhivardhan29 <78798859+Abhivardhan29@users.noreply.github.com>
2021-06-05 13:11:51 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -34,7 +34,7 @@ function draw(){
 }
 
     async function getBgImage(){
-        var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+        var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
         var responseJSON = await response.json();
         var dateTime = responseJSON.datetime;
         var hour = dateTime.slice(11,13);
